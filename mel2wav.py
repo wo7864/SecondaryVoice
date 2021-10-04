a6276686ef64b6dff00d6701bf6619d763ea267b
wo7864 <33424923+wo7864@users.noreply.github.com>
2021-10-05 02:06:07 +0900

    update

--- mel2wav.py
+++ mel2wav.py
@@ -21,6 +21,10 @@ from datasets import TextMelDataset, text_mel_collate
 from models import Generator
 from meldataset import MAX_WAV_VALUE
 
+from g2p_en import G2p
+from datasets.text import Language
+from modules.mel import mel_spectrogram
+
 synthesizer_path = 'pretrained_models/assem-vc_pretrained.ckpt' # path of synthesizer(VC decoder) checkpoint
 generator_path = 'pretrained_models/hifi-gan_vctk_g_02600000' # path of hifi-gan checkpoint
 config_path = 'assem-vc/hifi-gan/config_v1.json' # path of hifi-gan's configuration
@@ -44,7 +48,52 @@ state_dict_g = torch.load(generator_path, map_location='cpu')
 generator.load_state_dict(state_dict_g['generator'])
 generator.remove_weight_norm()
 
+g2p = G2p()
 print("Complete.")
+
+def script2text(script):
+    
+    phoneme = g2p(script)
+    converted = ['{']
+    for x in phoneme:
+        if x==' ':
+            converted.append('}')
+            converted.append('{')
+        elif x=='-':
+            continue
+        else:
+            converted.append(x)
+
+    converted.append('}')
+    phoneme = " ".join(str(x) for x in converted)
+    phoneme = phoneme.replace(' }', '}').replace('{ ','{')
+    phoneme = phoneme.replace('0','').replace('1','').replace('2','').replace('{\'}','\'').replace('{...}','...')
+    return phoneme.replace(' {!}','!').replace(' {?}','?').replace(' {.}','.').replace(' {,}',',')
+
+def get_mel_and_f0(audiopath, f0_mean=None, f0_var=None):
+    wav, sr = librosa.load(audiopath, sr=None, mono=True)
+    assert sr == hp.audio.sampling_rate, \
+        'sample mismatch: expected %d, got %d at %s' % (hp.audio.sampling_rate, sr, audiopath)
+    wav = torch.from_numpy(wav)
+    wav = wav.unsqueeze(0)
+
+    # wav = wav * (0.99 / (torch.max(torch.abs(wav)) + 1e-7))
+
+    mel = mel_spectrogram(wav, hp.audio.filter_length, hp.audio.n_mel_channels,
+                            hp.audio.sampling_rate,
+                            hp.audio.hop_length, hp.audio.win_length,
+                            hp.audio.mel_fmin, hp.audio.mel_fmax, center=False)
+    mel = mel.squeeze(0)
+    wav = wav.cpu().numpy()[0]
+    return mel
+
+def getSourceData(audiopath, script=None):
+
+    text = script2text(script)
+    lang = Language(hp.data.lang, hp.data.text_cleaners)
+    text_norm = torch.IntTensor(lang.text_to_sequence(text, hp.data.text_cleaners))
+    mel, f0 = get_mel_and_f0(audiopath)
+    return text_norm, mel, 0, 0, f0
 
 
 def mel2wav(src_num, target_mel, save_path):    
@@ -55,6 +104,9 @@ def mel2wav(src_num, target_mel, save_path):
     source_idx = src_num # 0 ~ len(source_metadata)-1
     audio_path, text,_ = sourceloader.meta[source_idx]
     x = sourceloader.__getitem__(source_idx)
+    print(x[0].size())
+    print(x[1].size())
+    print(x[4].size())
     batch = text_mel_collate([x])
     print("length of the source metadata is : ",len(sourceloader))
 
--- synthesis.py
+++ synthesis.py
@@ -7,7 +7,7 @@ from mel2wav import mel2wav
 
 if __name__ == "__main__":
     src_audio_num = 0
-    target_audio_path = 'example/p225_005-22k.wav'
+    target_audio_path = 'example/a001.wav'
     save_path = target_audio_path.split('/')[-1].split('.')[0] + '.wav'
     mel = wav2mel(target_audio_path)
     audio = mel2wav(src_audio_num, mel, save_path)
